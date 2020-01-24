97eade3d40e0b874403aec3802c81abc72bd46d2
yutveg <kodi.0n31l@gmail.com>
2020-01-24 09:06:09 -0800

    endpoints up for testing

--- api/projects/project-router.js
+++ api/projects/project-router.js
@@ -1,5 +1,38 @@
 const express = require("express");
 
 const router = express.Router();
+const Projects = require("./project-model.js");
+
+router.get("/api/projects", (req, res) => {
+  Projects.getProjects().then(result => {
+    if (result) res.status(200).json(result);
+    else res.status(500).json({ error: "end me" });
+  });
+});
+
+router.post("/api/projects", (req, res) => {
+  Projects.addProject(req.body)
+    .then(result => {
+      if (result) res.status(200).json(result);
+      else res.status(500).json({ error: "end me" });
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
+});
+
+router.get("/api/projects/:id/tasks", (req, res) => {
+  Projects.getTasks().then(result => {
+    if (result) res.status(200).json(result);
+    else res.status(500).json({ error: "end me" });
+  });
+});
+
+router.post("/api/projects/:id/tasks", (req, res) => {
+  Projects.addTask(req.params.id, req.body).then(result => {
+    if (result) res.status(200).json(result);
+    else res.status(500).json({ error: "end me" });
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const server = express();
+const projectRouter = require("./api/projects/project-router.js");
+const resourceRouter = require("./api/resources/resource-router.js");
 
 server.use(express.json());
 server.use("/api/projects", projectRouter);
